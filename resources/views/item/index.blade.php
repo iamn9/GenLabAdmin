4c8d9dc338d133d3ab8c708921cdc4bec0083ade
Noel Garcia <nagarcia2@up.edu.ph>
2016-12-11 23:01:26 +0800

    Visual Update for Item Index and User Index
    *Both interfaces are now the same
    *Search box added
    *User Index has status boxes now.

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -3,13 +3,20 @@
 @section('content')
 
 <section class="content">
-    <h1>
-        Item Index
-    </h1>
+<div class="box box-primary">
+<div class="box-header">
+    <h1>Item Index</h1>
+        <div class="input-group" >
+        <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+        <div class="input-group-btn">
+            <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+        </div>
+    </div>
+</div>
+<div class="box-body">
     <form class = 'col s3' method = 'get' action = '{!!url("item")!!}/create'>
         <button class = 'btn btn-primary' type = 'submit'>Create New item</button>
     </form>
-    <br>
     <br>
     <table class = "table table-striped table-bordered table-hover" style = 'background:#fff'>
         <thead>
@@ -32,5 +39,7 @@
         </tbody>
     </table>
     {!! $items->render() !!}
+</div>
+</div>
 </section>
 @endsection
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,7 +3,13 @@
 <section class="content">
 <div class="box box-primary">
 <div class="box-header">
-	<h3>All Users</h3>
+	<h1>All Users</h1>
+	<div class="input-group" >
+        <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+        <div class="input-group-btn">
+            <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+        </div>
+    </div>
 </div>
 	<div class="box-body">
 		<a href="{{url('/admin/users/create')}}" class = "btn btn-success"><i class="fa fa-plus fa-md" aria-hidden="true"></i> New</a>
@@ -11,6 +17,7 @@
 		<thead>
 			<th>Name</th>
 			<th>Email</th>
+			<th>Status</th>
 			<th>Actions</th>
 		</thead>
 		<tbody>
@@ -18,6 +25,16 @@
 			<tr>
 				<td>{{$user->name}}</td>
 				<td>{{$user->email}}</td>
+				<td>
+					@if($user->isAdmin)
+						<span class="label label-success">Admin</span>
+					@endif
+					@if($user->isActivated)
+						<span class="label label-primary">Activated</span>
+					@else
+						<span class="label label-warning">Not Activated</span>
+					@endif
+				</td>
 				<td>
 					<a href="{{url('/admin/users/edit')}}/{{$user->id}}" class = 'btn btn-primary btn-sm'><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
 					<a href="{{url('admin/users/delete')}}/{{$user->id}}" class = "btn btn-danger btn-sm"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
