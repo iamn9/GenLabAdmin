266d5142979ff3d9c0ab89279337f14a63c222b9
Noel Garcia <nagarcia2@up.edu.ph>
2016-12-19 11:31:06 +0800

    Fix for delete button Admin Management of Items

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -32,7 +32,7 @@
                 <td>{!!$item->name!!}</td>
                 <td>{!!$item->description!!}</td>
                 <td>
-                    <a data-toggle="modal" data-target="#myModal" class = 'delete btn btn-danger btn-xs' data-link = "/item/{!!$item->id!!}/deleteMsg" ><i class = 'material-icons'>delete</i></a>
+                    <a href= '/item' class = 'delete btn btn-danger btn-xs' data-link = "/item/{!!$item->id!!}/delete" ><i class = 'material-icons'>delete</i></a>
                     <a href = '#' class = 'viewEdit btn btn-primary btn-xs' data-link = '/item/{!!$item->id!!}/edit'><i class = 'material-icons'>edit</i></a>
                     <a href = '#' class = 'viewShow btn btn-warning btn-xs' data-link = '/item/{!!$item->id!!}'><i class = 'material-icons'>info</i></a>
                 </td>
