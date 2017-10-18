c9f196c1c4d698dee0d7ed9fdbba1a60de3e7c56
Noel Garcia <nagarcia2@up.edu.ph>
2017-10-18 10:40:51 +0800

    Fix for User Cart

--- resources/views/cart/user.blade.php
+++ resources/views/cart/user.blade.php
@@ -35,49 +35,12 @@
             <br>
         @if(!is_null($cart_id))
         <div class="input-group">    
-            <button id = "myBtn" class = 'btn btn-success'>CHECKOUT</button>
+        <form method = 'GET' action = '/cart/{{$cart_id}}/checkout'>
+            <button class = 'btn btn-success'>CHECKOUT</button>
+        </form>
         @endif
-        <div class="input-group" >
-            <input type="text" name="search" class="form-control pull-right" placeholder="Search" value='{!!$searchWord!!}'>
-            <div class="input-group-btn">
-                <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-            </div>
         </div>
-    </form>
-</div>
-<div class="box-body">
-    <table class = "table table-striped table-bordered table-hover" style = 'background:#fff'>
-        <thead>
-            <th>item_id</th>
-            <th>name</th>
-            <th>qty</th>
-            <th>actions</th>
-        </thead>
-        <tbody>
-            @foreach($cart_items as $cart_item) 
-            <tr>
-                <td>{!!$cart_item->item_id!!}</td>
-                <td>{!!$cart_item->name!!}</td>
-                <td>{!!$cart_item->qty!!}</td>
-                <td>
-                    <a href = '#' data-toggle="modal" data-target="#myModal" class = 'delete btn btn-danger btn-xs' data-link = "/cart_item/{!!$cart_item->id!!}/deleteMsg" ><i class="fa fa-trash-o" aria-hidden="true"></i>  Remove</a>
-                    <a href = '#' class = 'viewEdit btn btn-warning btn-xs' data-link = '/cart_item/{!!$cart_item->id!!}/edit'><i class="fa fa-pencil-square-o" aria-hidden="true"></i>  Edit</a>
-                    <a href = '#' data-toggle="modal" data-target="#myModal" class = 'delete btn btn-primary btn-xs' data-link = "/item/{!!$cart_item->item_id!!}/showModal" ><i class="fa fa-info" aria-hidden="true"></i>  Item Info</a>
-                </td>
-            </tr>
-            @endforeach 
-        </tbody>
-    </table>
-    <div class='text-center'>{!! $cart_items->render() !!}</div>
-        <br>
-    @if(!is_null($cart_id))
-    <div class="input-group">    
-    <form method = 'GET' action = '/cart/{{$cart_id}}/checkout'>
-        <button class = 'btn btn-success'>CHECKOUT</button>
-    </form>
-    @endif
     </div>
-</div>
 </div>
 </section>
 @endsection
