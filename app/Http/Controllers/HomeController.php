64b800a930e370492955f9b103d7f165614f1df8
Noel Garcia <nagarcia2@up.edu.ph>
2017-11-29 10:59:33 +0800

    Summary Stats on User Dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -40,11 +40,14 @@ class HomeController extends Controller
             $transactions = DB::table('transactions')->join('carts','transactions.cart_id' ,'=', 'carts.id')->join('users','carts.borrower_id','=','users.id_no')->limit(5)->get();
             return view('admin_dashboard',compact('transactions','count_unactivatedUsers','count_newOrders','count_completed','news'));
         }
-            else  
-            {
-                $cur_user = (Auth::user()->id_no);
-                $transactions = DB::table('transactions')->join('carts','transactions.cart_id' ,'=', 'carts.id')->join('users','carts.borrower_id','=','users.id_no')->where('users.id_no','=', $cur_user)->limit(5)->get();
-                return view('user_dashboard', compact('transactions','news'));
-            }
+        else  
+        {
+            $cur_user = (Auth::user()->id_no);
+            $count_pendingCarts = DB::table('carts')->where('borrower_id',$cur_user)->where('status','Pending')->count();
+            $count_unreturnedCarts = DB::table('carts')->where('borrower_id',$cur_user)->where('status','Released')->count();
+            $count_readyCarts = DB::table('carts')->where('borrower_id',$cur_user)->where('status','Prepared')->count();
+            $transactions = DB::table('transactions')->join('carts','transactions.cart_id' ,'=', 'carts.id')->join('users','carts.borrower_id','=','users.id_no')->where('users.id_no','=', $cur_user)->limit(5)->get();
+            return view('user_dashboard', compact('transactions','news','count_pendingCarts','count_unreturnedCarts','count_readyCarts'));
+        }
     }
 }
