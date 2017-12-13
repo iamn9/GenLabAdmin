6921fa4745178449a234c49a7ba4a247eb7c9019
Sudo-nim <edcel586@gmail.com>
2017-12-13 17:37:45 +0800

    Set list of latest transactions to descending order.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,14 +30,12 @@ class HomeController extends Controller
     public function index()
     {
         $news = News::join('users', function ($join) {
-            $join->on('news.reporter_id', '=', 'users.id_no');})
-            ->select('news.content', 'users.name','news.date_posted')
-            ->orderBy('news.date_posted','desc')->limit(5)->get();
+            $join->on('news.reporter_id', '=', 'users.id_no');})->select('news.content', 'users.name','news.date_posted')->orderBy('news.date_posted','desc')->limit(5)->get();
         if (Auth::user()->isAdmin){
             $count_unactivatedUsers = DB::table('users')->where('isActivated','0')->count();
             $count_newOrders = DB::table('carts')->where('status','Pending')->count();
             $count_completed = DB::table('carts')->where('status','Completed')->count();
-            $transactions = DB::table('transactions')->join('carts','transactions.cart_id' ,'=', 'carts.id')->join('users','carts.borrower_id','=','users.id_no')->limit(5)->get();
+            $transactions = DB::table('transactions')->join('carts','transactions.cart_id' ,'=', 'carts.id')->join('users','carts.borrower_id','=','users.id_no')->orderBy('transactions.submitted_at', 'desc')->limit(5)->get();
             return view('admin_dashboard',compact('transactions','count_unactivatedUsers','count_newOrders','count_completed','news'));
         }
             else  
