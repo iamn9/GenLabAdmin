cb68bc64d50a0a005a1b0c97bfc7cbe840ef424a
Noel Garcia <nagarcia2@up.edu.ph>
2018-01-31 14:12:35 +0800

    Fix for Create Item Feature

--- resources/views/item/create.blade.php
+++ resources/views/item/create.blade.php
@@ -6,48 +6,60 @@
 <div class="box-header">
     <h1>{{$title}}</h1>
     <form method = 'get' action = '{!!url("item")!!}'>
-        <button class = 'btn btn-danger'>item Index</button>
+        <button class = 'btn btn-danger'>Item Index</button>
     </form>
 </div>
 <div class="box-body">
-    <br>
-    <form method = 'POST' action = '{!!url("item")!!}'>
-        <input type = 'hidden' name = '_token' value = '{{Session::token()}}'>
-        <div class="form-group">
-            <label for="name">name</label>
-            <input id="name" name = "name" type="text" class="form-control" required>
+<br>
+<form method = 'POST' action = 'http://localhost:8000/item'>
+    <input type = 'hidden' name = '_token' value = '{{Session::token()}}'>
+    <div class="form-group">
+        <label for="name">Name</label>
+        <input id="name" name = "name" type="text" class="form-control" required>
+    </div>
+    <div class="form-group">
+        <label class="control-label mb-10">Brand</label>
+        <input type="text" name="brand" id="brand" class="form-control" placeholder="">
+    </div>
+    <div class="form-group">
+        <label for="description">Description</label>
+        <input id="description" name = "description" type="text" class="form-control" required>
+    </div>
+    <div class="form-group">
+        <label class="control-label mb-10">Quantity</label>
+        <input type="number" name="quantity" id="quantity" class="form-control" min="0" value="1">
+    </div>
+    <div class="form-group">
+        <label class="control-label mb-10">Wattage</label>
+        <input type="number" name="wattage" id="wattage" class="form-control" placeholder="0.00" value="0">
+    </div>
+    <div class="form-group">
+        <label class="control-label mb-10">Acquisition cost</label>
+        <div class="input-group">
+            <span class="input-group-addon">PHP</span>
+            <input type="number" name="acquisitioncost" id="acquisitioncost" class="form-control" placeholder="0.00" value="0.00" step=".01">
         </div>
-        <div class="form-group">
-            <label for="brand">brand</label>
-            <input id="brand" name = "brand" type="text" class="form-control">
+    </div>
+    <div class="form-group">
+        <label class="control-label mb-10">First hour rate</label>
+        <div class="input-group">
+            <span class="input-group-addon">PHP</span>
+            <input type="number" name="firsthour" id="firsthour" class="form-control" placeholder="0.00" value="0.00" step=".01">
         </div>
-        <div class="form-group">
-            <label for="quantity">quantity</label>
-            <input id="quantity" name = "quantity" type="text" class="form-control">
+    </div>
+    <div class="form-group">
+        <label class="control-label mb-10">Succeeding hour rate</label>
+        <div class="input-group">
+            <span class="input-group-addon">PHP</span>
+            <input type="number" name="succeeding" id="succeeding" class="form-control" placeholder="0.00" value="0.00" step=".01">
         </div>
-        <div class="form-group">
-            <label for="acquisition_cost">acquisition</label>
-            <input id="acqusition_cost" name = "acquisitioncost" type="text" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="wattage">wattage</label>
-            <input id="wattage" name = "wattage" type="text" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="firsthour">firsthour</label>
-            <input id="firsthour" name = "firsthour" type="text" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="succeeding">succeeding</label>
-            <input id="succeeding" name = "succeeding" type="text" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="description">description</label>
-            <input id="description" name = "description" type="text" class="form-control" required>
-        </div>
-
-        <button class = 'btn btn-primary' type ='submit'>Create</button>
-    </form>
+    </div>
+    <div class="form-group">
+        <label for="exampleInputFile">Upload Image</label>
+        <input data-toggle="tooltip" title="Feature not yet available." type="file" id="image" disabled>
+    </div>
+    <button class = 'btn btn-primary' type ='submit'>Create</button>
+</form>
 </div>
 </div>
 @endsection
