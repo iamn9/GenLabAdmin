7bfa441464ac8ccac41d2fc0f025e02941ae51e9
Noel Garcia <nagarcia2@up.edu.ph>
2018-02-04 10:32:33 +0800

    Updated Items Index UI and sanitized code

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -10,122 +10,19 @@
 
 <div class="box-body">
     <form class = 'col s3' method = 'get' action = '{!!url("item")!!}/create'>
-        <button class = 'btn btn-primary addBtn' type = 'submit'>Create New item</button>
-		<a data-toggle="modal" data-target="#addBtn" class = 'upload btn btn-primary'  data-link = "" ><i aria-hidden="true"></i> Add New Item</a>	      
-		<a data-toggle="modal" data-target="#uploadBtn" class = 'upload btn btn-primary'  data-link = "/item/1/uploadMsg" ><i aria-hidden="true"></i>Import</a>	  
+      <button class = 'btn btn-primary addBtn' type = 'submit'><i class="fa fa-plus fa-md" aria-hidden="true"></i>  Add New Item</button>
+			<a data-toggle="tooltip" title="Feature not yet available." class = 'upload btn btn-primary' href='#' disabled> Import from file</a>
     </form>		
-  
-		 <div id="addBtn" class="modal fade in" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		  <div class="modal-dialog">
-			<div class="modal-content">
-			  <div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-				<h5 class="modal-title" id="myModalLabel">Add an equipment</h5>
-			  </div>
-			  <div class="modal-body">
-				<form>
-				  <div class="form-group">
-					<label class="control-label mb-10">Equipment Name</label>
-					<select class="form-control" name="medicine_id" id="medicine_id">
-					  @foreach($items as $item)
-					  <option value="{{$item->id}}" >{{$item->generic_name}}</option>
-					  @endforeach
-					</select>
-				  </div>
 
-				  <div class="form-group">
-					<label class="control-label mb-10">Brand</label>
-					<input type="text" name="dosage_name" id="dosage-volume" class="form-control" placeholder="">
-				  </div>
-
-				  <div class="form-group">
-					<label class="control-label mb-10">Quantity</label>
-					<input type="text" name="form" id="form" class="form-control" placeholder="0">
-				  </div>
-
-				  <div class="form-group">
-					<label class="control-label mb-10">Acquisition cost</label>
-					<input type="text" name="price" id="price" class="form-control" placeholder="0.00 php">
-				  </div>
-				  
-				  <div class="form-group">
-					<label class="control-label mb-10">Wattage</label>
-					<input type="text" name="price" id="price" class="form-control" placeholder="0.00">
-				  </div>
-
-				  <div class="form-group">
-					<label class="control-label mb-10">First hour rate</label>
-					<input type="text" name="price" id="price" class="form-control" placeholder="0.00 php">
-				  </div>
-		
-				<div class="form-group">
-					<label class="control-label mb-10">Succeeding hour rate</label>
-					<input type="text" name="price" id="price" class="form-control" placeholder="0.00 php">
-				  </div>
-
-
-				  <div class="form-group">
-					<label class="control-label mb-10">Insert a photo</label>
-					<div class="panel-wrapper collapse in">
-					  <div class="panel-body">
-						<div class="mt-20">
-						  <input type="file" name="photo" id="input-file-now" class="dropify" >
-						</div>  
-					  </div>
-					</div>
-				  </div>
-
-				</form>
-			  </div>
-			  <div class="modal-footer">
-				<button type="button" class="btn btn-success waves-effect" data-dismiss="modal" id="save-dosage">Save</button>
-				<button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Cancel</button>
-			  </div>
-
-			</div>
-			<!-- /.modal-content -->
-		  </div>
-		</div>
-		
-		<div id="uploadBtn" class="modal fade in" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		  <div class="modal-dialog">
-			<div class="modal-content">
-			  <div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-				<h5 class="modal-title" id="myModalLabel"><strong>Upload a file</strong></h5>
-			  </div>
-			  <div class="modal-body">
-				<form>				  				 
-				  <div class="form-group">					
-					<div class="panel-wrapper collapse in">
-					  <div class="panel-body">
-						<div class="mt-20">
-						  <input type="file" name="file" id="input-file-now" class="dropify" >
-						</div>  
-					  </div>
-					</div>
-				  </div>
-
-				</form>
-			  </div>
-			  <div class="modal-footer">
-				<button type="button" class="btn btn-success waves-effect" data-dismiss="modal" id="save-dosage">Save</button>
-				<button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Cancel</button>
-			  </div>
-
-			</div>
-			<!-- /.modal-content -->
-		  </div>
-		</div>
-	
+		<br/>
     <table class = "table table-striped table-bordered table-hover" style = 'background:#fff'>
         <thead>
-          <th style="width: 60px">id</th>
-          <th style="width: 150px">name</th>
-          <th style="width: 150px">brand</th>
-          <th style="width: 100px">quantity</th>
-            <th>description</th>
-            <th style="width: 170px">actions</th>
+					<th style="width: 60px">id</th>
+					<th style="width: 200px">name</th>
+					<th style="width: 150px">brand</th>
+					<th>description</th>
+					<th style="width: 60px">qty</th>
+					<th style="width: 180px">actions</th>
         </thead>
         <tbody>
             @foreach($items as $item)
@@ -133,8 +30,8 @@
                 <td>{!!$item->id!!}</td>
                 <td>{!!$item->name!!}</td>
                 <td>{!!$item->brand!!}</td>
+								<td>{!!$item->description!!}</td>
                 <td>{!!$item->quantity!!}</td>
-                <td>{!!$item->description!!}</td>
                 <td>
                     <a data-toggle="tooltip" title="Remove the Item" href = '#' data-toggle="modal" data-target="#myModal" class = 'delete btn btn-danger btn-xs' data-link = "/item/{!!$item->id!!}/deleteMsg" ><i class="fa fa-trash-o" aria-hidden="true"></i>  Delete</a>
                     <a data-toggle="tooltip" title="Edit Item Information" class = 'viewEdit btn btn-primary btn-xs' href = '/item/{!!$item->id!!}/edit'><i class="fa fa-pencil-square-o" aria-hidden="true"></i>  Edit</a>
